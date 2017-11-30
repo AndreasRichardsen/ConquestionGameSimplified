c20d206d97661d948b8ace9dc3474a4bf8521bf3
MimiMicek <makaveli_girl1996@hotmail.com>
2017-11-30 16:23:00 +0100

    WebClient and Sprint3 notes

---
+++ ConquestionGame.Presentation.WebClient/Controllers/LogInController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConquestionGame.Presentation.WebClient.Controllers
+{
+    public class LogInController : Controller
+    {
+        // GET: LogIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
